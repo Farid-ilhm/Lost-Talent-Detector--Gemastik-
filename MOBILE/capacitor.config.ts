25b4328a7b37bd3f34af235081df69ebeee66710
Shoukoraa <josptrr@gmail.com>
2026-08-08 15:45:57 +0700

    nama apk

--- MOBILE/capacitor.config.ts
+++ MOBILE/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MOBILE',
+  appId: 'com.ltdetector.app',
+  appName: 'Lost Talent Detector',
   webDir: 'www'
 };
 
